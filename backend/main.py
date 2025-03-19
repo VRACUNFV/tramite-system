e9d12bf1073c50d04789f537a7e74a693fc29a66
VRACUNFV <ccamposm@unfv.edu.pe>
2025-03-19 16:36:58 -0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,12 @@
+from flask import Flask
+from api import api, models
+from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tramite_system.db'
+models.db.init_app(app)
+
+app.register_blueprint(api, url_prefix='/api')
+
+if __name__ == '__main__':
+    app.run(debug=True)

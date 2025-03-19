81f34b14b3a5a5796b041727387cd10e7570223e
VRACUNFV <ccamposm@unfv.edu.pe>
2025-03-19 16:38:23 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,13 @@
+import React from 'react';
 
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Sistema de Gestión de Trámites</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
